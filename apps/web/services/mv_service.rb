91489bdbd12286fc90daf57088fe3d9fe3ca81ec
Alex F <adfarries@gmail.com>
2018-07-21 17:17:12 +1000

    Add mv_service spec with fixture

--- apps/web/services/mv_service.rb
+++ apps/web/services/mv_service.rb
@@ -1,15 +1,27 @@
-module MVService
-  class Article
-    def initialize(per_page)
-      @per_page = per_page
-    end
+class Web::MVService
+  class ServiceError < StandardError; end
 
-    def root_uri
-      @root_uri ||= "https://collections.museumvictoria.com.au/api"
-    end
+  def initialize(per_page = nil)
+    @per_page &&= per_page
+  end
 
-    def search(queries)
-      queries.map { |query| "query=#{query.to_s}" }.join('&')
-    end
+  def request(params)
+    response = HTTP.get(root_uri + per_page + params)
+    raise ServiceError unless response.code == 200
+    Oj.load(response.to_s)
+  end
+
+  def search(queries)
+    request("/search?" + queries.map { |query| "query=#{query}" }.join('&'))
+  end
+
+  private
+
+  def root_uri
+    @root_uri ||= 'https://collections.museumvictoria.com.au/api'
+  end
+
+  def per_page
+    @per_page ? "&perpage=#{@per_page}" : ''
   end
 end
---
+++ spec/web/services/mv_service_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe Web::MVService do
+  subject { described_class.new }
+
+  describe '#search' do
+    context 'with multiple queries' do
+      let(:queries) { ['shoe', 'soldier'] }
+
+      it 'returns a parsed response after making a request to MV with queries as parameters' do
+        stub_service(:mv_search)
+        response = subject.search(queries)
+
+        expect(response.first.fetch("recordType")).to eql("item")
+        expect(response.first.fetch("category")).to eql("History & Technology")
+        expect(response.first.fetch("acquisitionInformation")).to eql("Donation from J. Lord, 24 Feb 1986")
+      end
+    end
+
+    context 'with no queries' do
+
+    end
+  end
+end
