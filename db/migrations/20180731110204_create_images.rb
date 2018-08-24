52606bed42449e009cf1fa124d137730a6edd5f3
Alex F <adfarries@gmail.com>
2018-08-24 20:17:16 +1000

    Prepare migration for creating images table
    * may want differing distinct uris in future

--- db/migrations/20180731110204_create_images.rb
+++ db/migrations/20180731110204_create_images.rb
@@ -2,6 +2,9 @@ Hanami::Model.migration do
   change do
     create_table :images do
       primary_key :id
+      foreign_key :article_id, :articles, null: false, unique: true
+
+      column :uri, String
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
