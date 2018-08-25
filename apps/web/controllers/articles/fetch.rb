ec22bc63fcbc7befa200263dd58115f31f5112da
Alex F <adfarries@gmail.com>
2018-08-25 16:31:02 +1000

    Separate creation of repository object and fetch

--- apps/web/controllers/articles/fetch.rb
+++ apps/web/controllers/articles/fetch.rb
@@ -5,9 +5,16 @@ module Web::Controllers::Articles
     include Web::Action
 
     def call(params)
-      # For now, will make a direct request to external endpoints, rather than sending it through the worker
+      # For now, will make a direct request to external endpoints,
+      # rather than sending it through the worker
       fetch_total = params[:fetch_total]
       page_total = (fetch_total.to_f / params[:per_page]).ceil
+      collect_articles(fetch_total, page_total)
+    end
+
+    private
+
+    def collect_articles(fetch_total, page_total)
       current_page = 1
       total_fetched = 0
 
@@ -15,14 +22,11 @@ module Web::Controllers::Articles
         articles = service.request('/articles', current_page)
         articles.each do |article|
           break if total_fetched >= fetch_total
-          ArticleRepository.new(attributes(article))
-          process_images(article)
+          ArticleRepository.create_with_images(article)
         end
-       current_page += 1
+        current_page += 1
       end
     end
-
-    private
 
     def service
       @service ||= Web::MVService.new(per_page)
