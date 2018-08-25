f7a17ba9701d53b6888b09deaa4a86d3a6b50d27
Alex F <adfarries@gmail.com>
2018-08-25 18:32:27 +1000

    Use text data type, not text array type
    * https://www.postgresql.org/docs/10/static/arrays.html

--- db/migrations/20180731110128_create_articles.rb
+++ db/migrations/20180731110128_create_articles.rb
@@ -3,9 +3,12 @@ Hanami::Model.migration do
     create_table :articles do
       primary_key :id
 
+      # TODO: at some point, may want to actually change these text
+      # types back to arrays: could be a more useful way to transport
+      # data client-side
       column :title, String, null: false
-      column :content, "text[]"
-      column :content_summary, "text[]"
+      column :content, "text"
+      column :content_summary, "text"
       column :types, String
       column :authors, String
 
