8cadd66409d8e0c51a1723037f7bdd1010b10981
Alex F <adfarries@gmail.com>
2018-08-25 17:16:24 +1000

    Add authors to article

--- db/migrations/20180731110128_create_articles.rb
+++ db/migrations/20180731110128_create_articles.rb
@@ -2,11 +2,12 @@ Hanami::Model.migration do
   change do
     create_table :articles do
       primary_key :id
-      
+
       column :title, String, null: false
       column :description, "text[]"
       column :category, String
       column :significance, "text[]"
+      column :authors, String
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
--- lib/ennigaldi/repositories/article_repository.rb
+++ lib/ennigaldi/repositories/article_repository.rb
@@ -12,20 +12,28 @@ class ArticleRepository < Hanami::Repository
 
   def attributes(article)
     {
-      title: article.fetch('objectName'),
-      description: article.fetch('objectSummary'),
+      title: article.fetch('title'),
+      description: article.fetch('contentSummary'),
       category: article.fetch('category'),
-      significance: article.fetch('significance')
+      significance: article.fetch('significance'),
+      authors: authors(article)
     }
   end
 
   def process_images(article, new_article)
     images = article.fetch('media').select { |m| m['type'] == 'image' }
-    images.each { |i| ImageRepository.new.create(new_article.id, uri(i)) }
+    images.each { |i| new_article.images.create(uri(i)) }
   end
 
   # default to medium for now
   def uri(image)
     image.dig('medium, uri')
+  end
+
+  def authors(article)
+    article
+      .fetch('authors')
+      .map { |a| a['fullName'] }
+      .join(', ')
   end
 end
