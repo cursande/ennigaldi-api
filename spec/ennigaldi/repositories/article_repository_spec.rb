55e7abb2cdeafb73b4253b81c78e123f0bf9184f
Alex F <adfarries@gmail.com>
2018-08-25 19:05:23 +1000

    Clean db between tests

--- spec/ennigaldi/repositories/article_repository_spec.rb
+++ spec/ennigaldi/repositories/article_repository_spec.rb
@@ -6,17 +6,8 @@ RSpec.describe ArticleRepository, type: :repository do
     let(:article_repository) { ArticleRepository.new }
     let(:image_repository) { ImageRepository.new }
 
-    # TODO: Move this kind of database sweep into spec helper
-    before do
-      article_repository.clear
-      image_repository.clear
-    end
-
     it 'saves an article and its associated images in the db' do
       subject.create_with_images(article_data)
-
-      article_repository = ArticleRepository.new
-      image_repository = ImageRepository.new
 
       expect(article_repository.all.length).to eql(1)
       expect(image_repository.all.length).to eql(1)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -102,4 +102,9 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
 end
