dd0ff46e37e665eb6e53176a8ae3e5ea53557609
Alex F <adfarries@gmail.com>
2018-08-25 16:32:59 +1000

    Add create_with_images to ArticleRepository

--- lib/ennigaldi/repositories/article_repository.rb
+++ lib/ennigaldi/repositories/article_repository.rb
@@ -1,2 +1,28 @@
 class ArticleRepository < Hanami::Repository
+
+  def create_with_images(article)
+    new_article = self.class.new.create(attributes(article))
+    process_images(article, new_article)
+  end
+
+  private
+
+  def attributes(article)
+    {
+      title: article.fetch('objectName'),
+      description: article.fetch('objectSummary'),
+      category: article.fetch('category'),
+      significance: article.fetch('significance')
+    }
+  end
+
+  def process_images(article, new_article)
+    images = article.fetch('media').select { |m| m['type'] == 'image' }
+    images.each { |i| ImageRepository.new.create(new_article.id, uri(i)) }
+  end
+
+  # default to medium for now
+  def uri(image)
+    image.dig('medium, uri')
+  end
 end
