b27b6d3e3f81e07bf0a87e861d9bb2286ca6ed53
Alex F <adfarries@gmail.com>
2018-09-01 23:22:57 +1000

    Add uploader

--- apps/web/application.rb
+++ apps/web/application.rb
@@ -24,6 +24,7 @@ module Web
         controllers
         views
         services
+        uploaders
       ]
 
       # Handle exceptions with HTTP statuses (true) or don't catch them (false).
--- lib/ennigaldi/repositories/image_repository.rb
+++ lib/ennigaldi/repositories/image_repository.rb
@@ -1,4 +1,6 @@
 class ImageRepository < Hanami::Repository
+  include ImageUploader::Attachment.new(:image)
+
   associations do
     belongs_to :article
   end
