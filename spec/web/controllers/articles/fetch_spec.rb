f7e2b8e21d6962697079c02b86a6bd7e78477a83
Alex F <adfarries@gmail.com>
2018-09-02 18:18:12 +1000

    Add basic spec and fixture for fetch

--- spec/web/controllers/articles/fetch_spec.rb
+++ spec/web/controllers/articles/fetch_spec.rb
@@ -1,17 +1,17 @@
 RSpec.describe Web::Controllers::Articles::Fetch, type: :action do
   let(:action) { described_class.new }
-  let(:params) do
-    {
-      fetch_total: 60,
-      per_page: 20
-    }
-  end
+  let(:article_repository) { ArticleRepository.new }
+  let(:image_repository) { ImageRepository.new }
+  let(:params) { fetch_total: 10 }
 
   describe '#call' do
     context 'with a page of articles' do
-
+      before { stub_service(:fetch_mv_articles) }
       it 'saves the specified number of articles' do
+        action.call(params)
 
+        articles = article_repository.all
+        expect(articles.count).to eql(3)
       end
     end
   end
