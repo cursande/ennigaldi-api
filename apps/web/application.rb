9497953f196155c5fbf6305d2ab124fab45f7a35
Alex F <adfarries@gmail.com>
2018-09-04 20:09:04 +1000

    Cut comments out of application.rb

--- apps/web/application.rb
+++ apps/web/application.rb
@@ -6,20 +6,8 @@ require 'hanami/assets'
 module Web
   class Application < Hanami::Application
     configure do
-      ##
-      # BASIC
-      #
-
-      # Define the root path of this application.
-      # All paths specified in this configuration are relative to path below.
-      #
       root __dir__
 
-      # Relative load paths where this application will recursively load the
-      # code.
-      #
-      # When you add new directories, remember to add them here.
-      #
       load_paths << %w[
         controllers
         views
@@ -30,16 +18,12 @@ module Web
       # Handle exceptions with HTTP statuses (true) or don't catch them (false).
       # Defaults to true.
       # See: http://www.rubydoc.info/gems/hanami-controller/#Exceptions_management
-      #
       # handle_exceptions true
 
-      ##
       # HTTP
-      #
 
       # Routes definitions for this application
       # See: http://www.rubydoc.info/gems/hanami-router#Usage
-      #
       routes 'config/routes'
 
       # URI scheme used by the routing system to generate absolute URLs
@@ -51,14 +35,6 @@ module Web
       # Defaults to "localhost"
       #
       # host 'example.org'
-
-      # URI port used by the routing system to generate absolute URLs
-      # Argument: An object coercible to integer, defaults to 80 if the scheme
-      # is http and 443 if it's https
-      #
-      # This should only be configured if app listens to non-standard ports
-      #
-      # port 443
 
       # Enable cookies
       # Argument: boolean to toggle the feature
