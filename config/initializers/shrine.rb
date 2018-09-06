65771b1a3208599bf11913c854e326272c1a10ac
Alex F <adfarries@gmail.com>
2018-09-06 18:11:47 +1000

    Requre in s3 lib for storage if env != test

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,15 +2,13 @@ require "shrine"
 
 if ENV['HANAMI_ENV'] == 'test'
   require 'shrine/storage/memory'
+
   Shrine.storages = {
     cache: Shrine::Storage::Memory.new,
     store: Shrine::Storage::Memory.new
   }
 else
-  Shrine.storages = {
-    cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
-    store: Shrine::Storage::S3.new(**s3_options),
-  }
+  require "shrine/storage/s3"
 
   s3_options = {
     bucket: ENV.fetch('S3_BUCKET'),
@@ -18,6 +16,12 @@ else
     secret_access_key: ENV.fetch('SECRET_ACCESS_KEY'),
     region: ENV.fetch('AWS_REGION')
   }
+
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+    store: Shrine::Storage::S3.new(**s3_options),
+  }
+
 end
 
 
